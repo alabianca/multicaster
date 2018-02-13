a173b04a29fc6792cff372399ac075272b8f399b
Alexander <alexander@alabianca.attlocal.net>
2018-02-12 21:40:53 -0600

    fixed test

--- test.js
+++ test.js
@@ -1,33 +1,38 @@
 
 const os = require('os');
-const myself = '192.168.1.220'
+//const myself = '192.168.1.220'
 const multicaster = require('./index')().multicaster
 const target = process.argv[2];
 
 const me = require('./lib/address');
 
 console.log(me())
+const myself = me();
 
 multicaster.on('response', (res)=>{
     //console.log('GOT A RESPONSE');
     //console.log(res.msg.answers);
     const answ = res.msg.answers[0];
-    if(answ && answ.name && answ.name === 'spacedrop') {
-        console.log(`Host Found: ${answ.data.target}: ${answ.data.port}`)
-        multicaster.stop();
-        process.exit(1);
+    if(myself !== res.from) {
+        if(answ && answ.name && answ.name === 'spacedrop') {
+            console.log(`Host Found: ${answ.data.target}: ${answ.data.port}`)
+            multicaster.stop();
+            process.exit(1);
+        }
     }
+    
 })
 
 
 multicaster.on('query', (query)=>{
-
+    
+    console.log('QUERY FROM: ', query.from)
     if(myself !== query.from) {
         console.log('RESPONDING TO: ' + query.from)
         multicaster.respond({
             name: 'spacedrop',
             port: 3000,
-            target: me()
+            target: myself
         })
     }
     
